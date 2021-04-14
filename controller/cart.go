865c58fe1a01d16be3ab418bcd55bf452f7da4b6
tzr2020 <zhonziran@gmail.com>
2021-04-14 18:53:36 +0800

    添加会员用户将产品添加到购物车时，产品库存的判断功能

--- controller/cart.go
+++ controller/cart.go
@@ -19,7 +19,7 @@ func regsiterCartRoutes() {
 }
 
 // AddToCart 将产品添加到购物车
-// 整体流程：用户是否已经登录->数据库是否存在购物车->数据库是否存在购物项
+// 整体流程：用户是否已经登录->数据库是否存在购物车->数据库是否存在购物项->产品是否还有库存
 func AddToCart(w http.ResponseWriter, r *http.Request) {
 	ok, sess := IsLogin(r)
 
@@ -40,34 +40,25 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 		// 从数据库获取购物车
 		c, err := model.GetCartByUserID(uid)
 
-		// test
-		// fmt.Println("购物车: ", c)
-		// if c != nil {
-		// 	for k, v := range c.CartItems {
-		// 		fmt.Printf("第%v个购物项的信息: %v，产品信息：%v\n", k+1, v, v.Product)
-		// 	}
-		// }
-
 		if c != nil {
 			// 用户在数据库里已有购物车
 
 			// 从数据库获取某产品购物项
 			cItem, err := model.GetCartItemByCartIDAndProductID(c.CartID, pid)
-			// tset
-			// fmt.Println("购物项: ", cItem)
 			if err != nil && err != sql.ErrNoRows {
 				log.Println("从数据库获取购物项发生错误: ", err)
 				http.Error(w, util.ErrServerInside.Error(), http.StatusInternalServerError)
 				return
 			}
-
 			if cItem != nil {
 				// 购物车已有该产品对应的购物项，该产品的数量加1
 				for _, v := range c.CartItems {
 					if v.Product.ID == cItem.Product.ID {
+						if p.Stock == 0 {
+							w.Write([]byte("产品: " + p.Name + " 无货，正在补货中"))
+							return
+						}
 						v.Count += 1
-						// test
-						// fmt.Println(v.Count, v.Product.ID, c.CartID)
 						err := model.UpdateProductCountOfCartItem(v)
 						if err != nil {
 							log.Println("数据库更新购物项的产品数量发生错误: ", err)
@@ -77,9 +68,11 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 					}
 				}
 			} else {
-				// test
-				// fmt.Println("产品: ", p)
 				// 购物车还没有该产品对应的购物项，创建购物项添加到数据库，并维护购物车的购物项字段
+				if p.Stock == 0 {
+					w.Write([]byte("产品: " + p.Name + " 无货，正在补货中"))
+					return
+				}
 				cItem := &model.CartItem{
 					CartID:  c.CartID,
 					Product: p,
@@ -94,14 +87,6 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 				c.CartItems = append(c.CartItems, cItem)
 			}
 
-			// test
-			// fmt.Println("更新前的购物车：", c)
-			// if c != nil {
-			// 	for k, v := range c.CartItems {
-			// 		fmt.Printf("第%v个购物项的信息: %v，产品信息：%v\n", k+1, v, v.Product)
-			// 	}
-			// }
-
 			// 无论有没有购物车有没有该产品对应的购物项，最后都需要更新购物车
 			err = model.UpdateCountAndAmountOfCart(c)
 			if err != nil {
@@ -109,9 +94,8 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, util.ErrServerInside.Error(), http.StatusInternalServerError)
 				return
 			}
-
 		} else {
-			// 用户在数据库里还没购物车，数据库新增购物车
+			// 用户在数据库里还没购物车，数据库新增购物车和购物项
 			cid := util.CreateUUID()
 
 			cItem := &model.CartItem{
@@ -136,9 +120,7 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		}
-
 		w.Write([]byte("产品: " + p.Name + " 已加入购物车"))
-
 	} else {
 		// 用户还没登录
 		w.Write([]byte("请先登录账号，再将产品加入购物车"))
